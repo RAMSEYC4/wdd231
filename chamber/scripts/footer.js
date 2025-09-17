7a41a16c8df01ac7ac3ece29f0ea2beb2c348ec1
RAMSEYC4 <ramseycedou68@gmail.com>
2025-09-17 18:50:46 +0200

    2025 - 09 - 17 fix errors

---
+++ chamber/scripts/footer.js
@@ -0,0 +1,15 @@
+const year = new Date().getFullYear()
+const day = new Date().getDay();
+const month = new Date().getMonth();
+const hours = new Date().getHours();
+const minutes = new Date().getMinutes();
+const seconds = new Date().getSeconds();
+
+const lastModificationDate = new Date(document.lastModified)
+
+document.querySelector('#lastModified')
+  .innerHTML = `@${year} Cedou Ramsey Pretoria pretoria SA`
+
+document.querySelector('#currentyear')
+  .innerHTML = `last Modification : ${day}/${month}/${year} 
+                ${hours}/${minutes}/${seconds}`
